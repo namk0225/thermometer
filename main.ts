d891a75afd499d91cfc5ca3e0564770db0bb49c5
namk0225 <69349621+namk0225@users.noreply.github.com>
2020-08-12 14:14:25 +0900

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
-basic.forever(function () {
-    basic.showNumber(0)
+input.onButtonPressed(Button.A, function () {
+    strip.setBrightness(255)
+    basic.showNumber(input.temperature() % 10)
+    for (let index = 0; index <= 4; index++) {
+        strip.setPixelColor(index, neopixel.colors(NeoPixelColors.Red))
+    }
 })
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P1, 12, NeoPixelMode.RGB)
+let range = strip.range(0, (input.temperature() - input.temperature() % 10) / 10)
